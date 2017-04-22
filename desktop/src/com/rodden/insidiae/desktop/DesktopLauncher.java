35c6926992cbfb14e7b7f02fa8391aefec1269a3
David-Rodden <davidrodden273@gmail.com>
2017-04-21 23:47:18 -0700

    Initialization of Insidiae

---
+++ desktop/src/com/rodden/insidiae/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.rodden.insidiae.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.rodden.insidiae.Insidiae;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Insidiae(), config);
+	}
+}
